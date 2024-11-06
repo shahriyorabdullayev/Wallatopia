50f5719b05d84489e3b56c2bb9406ebf0378e22f
shahriyor <shahriyorabdullayev2000@gmail.com>
2024-11-06 16:33:31 +0500

    added Screens.kt

---
+++ composeApp/src/commonMain/kotlin/uz/droid/wallatopia/Screens.kt
@@ -0,0 +1,18 @@
+package uz.droid.wallatopia
+
+import kotlinx.serialization.Serializable
+
+object Screens {
+
+    @Serializable
+    object HomeScreen
+
+    @Serializable
+    object CategoryScreen
+
+    @Serializable
+    object FavouriteScreen
+
+    @Serializable
+    object ProfileScreen
+}
