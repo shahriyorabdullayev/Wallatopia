9720f6f9ee9df6f3126d59de8bc7487216797834
JonibekXolmonov <pendragon05082002@gmail.com>
2025-01-14 17:28:22 +0500

    search app bar conflict fixed

--- composeApp/src/commonMain/kotlin/uz/droid/wallatopia/presentation/components/SearchTopBar.kt
+++ composeApp/src/commonMain/kotlin/uz/droid/wallatopia/presentation/components/SearchTopBar.kt
@@ -2,20 +2,17 @@ package uz.droid.wallatopia.presentation.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
@@ -28,23 +25,18 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import org.jetbrains.compose.resources.DrawableResource
 import androidx.compose.ui.window.Popup
-import coil3.Extras
 import org.jetbrains.compose.resources.painterResource
 import uz.droid.wallatopia.common.resources.Drawables
 import uz.droid.wallatopia.common.theme.AppTheme
-import uz.droid.wallatopia.data.network.response.SuggestionResponse
 
 @Composable
 fun SearchTopBar(
@@ -64,7 +56,6 @@ fun SearchTopBar(
     LaunchedEffect(Unit) {
         focusRequester.requestFocus()
     }
-
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier.then(modifier)
@@ -76,31 +67,75 @@ fun SearchTopBar(
                 tint = AppTheme.colorScheme.immutableWhite
             )
         }
-        AppSearchField(
-        Icon(
-            painter = painterResource(Drawables.Icons.Back),
-            contentDescription = title,
-            tint = AppTheme.colorScheme.immutableWhite
-        )
         Column(
             modifier = Modifier
-                .weight(1f)
-                .advancedShadow(
-                    blur = 7.dp
+                .advancedShadow(blur = 7.dp)
+                .clip(AppTheme.shape.rounded10)
+                .background(AppTheme.colorScheme.jetGray)
+        ) {
+            AppSearchField(
+                modifier = Modifier,
+                textFieldModifier = Modifier.focusRequester(focusRequester),
+                value = query,
+                endIcon = {
+                    Icon(
+                        painter = painterResource(Drawables.Icons.Search),
+                        contentDescription = "Search",
+                        tint = AppTheme.colorScheme.chineseSilverGray
+                    )
+                },
+                endIconClickEnabled = true,
+                onValueChange = {
+                    onValueChange(it)
+                    showSuggestions = it.length > 2
+                },
+                hintText = "Search For Wallpapers",
+                onDoneAction = onSearch,
+                keyboardActions = KeyboardActions(
+                    onSearch = {
+                        onSearch(query)
+                        focusManager.clearFocus()
+                        keyboardController?.hide()
+                    }
                 ),
-            value = query,
-            endIcon = {
-                Icon(
-                    painter = painterResource(Drawables.Icons.Search),
-                    contentDescription = "Search",
-                    tint = AppTheme.colorScheme.chineseSilverGray
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Search
                 )
-            },
-            endIconClickEnabled = true,
-            onValueChange = onValueChange,
-            hintText = "Search For Wallpapers",
-            onDoneAction = onSearch
-        )
+            )
+            if (showSuggestions) {
+                Popup(
+                    alignment = Alignment.TopCenter,
+                    offset = IntOffset(x = 0, y = statusBarHeight.value.toInt() + 100),
+                    onDismissRequest = {
+                        showSuggestions = false
+                    }
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .advancedShadow(blur = 5.dp)
+                            .background(
+                                AppTheme.colorScheme.jetGray,
+                                shape = AppTheme.shape.rounded10
+                            )
+                            .width(280.dp)
+                            .padding(vertical = 4.dp)
+                    ) {
+                        suggestions.forEach { suggestion ->
+                            SuggestionItem(
+                                suggestion = suggestion,
+                                onClick = {
+                                    onValueChange(suggestion)
+                                    focusManager.clearFocus()
+                                    keyboardController?.hide()
+                                    showSuggestions = false
+                                    onSearch(query)
+                                }
+                            )
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -108,6 +143,9 @@ fun SearchTopBar(
 fun AppSearchField(
     enabled: Boolean = true,
     modifier: Modifier = Modifier,
+    textFieldModifier: Modifier = Modifier,
+    keyboardActions: KeyboardActions = KeyboardActions(),
+    keyboardOptions: KeyboardOptions = KeyboardOptions(),
     endIcon: @Composable () -> Unit,
     value: String,
     hintText: String,
@@ -127,7 +165,7 @@ fun AppSearchField(
             enabled = enabled,
             value = value,
             onValueChange = onValueChange,
-            modifier = Modifier.weight(1f),
+            modifier = Modifier.then(textFieldModifier).weight(1f),
             decorationBox = { innerTextField ->
                 if (value.isEmpty()) {
                     Text(
@@ -140,48 +178,10 @@ fun AppSearchField(
             },
             textStyle = AppTheme.typography.inputText.copy(
                 color = AppTheme.colorScheme.chineseSilverGray
-            )
+            ),
+            keyboardActions = keyboardActions,
+            keyboardOptions = keyboardOptions
         )
-                .advancedShadow(blur = 7.dp)
-                .clip(AppTheme.shape.rounded10)
-                .background(AppTheme.colorScheme.jetGray)
-        ) {
-            Row(
-                modifier = Modifier
-                    .padding(start = 18.dp, end = 8.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                BasicTextField(
-                    value = query,
-                    onValueChange = {
-                        onValueChange(it)
-                        showSuggestions = it.length > 2
-                    },
-                    modifier = Modifier.weight(1f).focusRequester(focusRequester),
-                    decorationBox = { innerTextField ->
-                        if (query.isEmpty()) {
-                            Text(
-                                "Search For Wallpapers",
-                                color = AppTheme.colorScheme.ashGray,
-                                style = AppTheme.typography.hintText
-                            )
-                        }
-                        innerTextField()
-                    },
-                    textStyle = AppTheme.typography.inputText.copy(
-                        color = AppTheme.colorScheme.chineseSilverGray
-                    ),
-                    keyboardActions = KeyboardActions(
-                        onSearch = {
-                            onSearch(query)
-                            focusManager.clearFocus()
-                            keyboardController?.hide()
-                        }
-                    ),
-                    keyboardOptions = KeyboardOptions(
-                        imeAction = ImeAction.Search
-                    )
-                )
 
         IconButton(
             enabled = endIconClickEnabled,
@@ -189,40 +189,8 @@ fun AppSearchField(
                 onDoneAction(value)
             }
         ) {
-                endIcon()
-            }
-            if (showSuggestions) {
-                Popup(
-                    alignment = Alignment.TopCenter,
-                    offset = IntOffset(x = 0, y = statusBarHeight.value.toInt()+100),
-                    onDismissRequest = {
-                        showSuggestions = false
-                    }
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .advancedShadow(blur = 5.dp)
-                            .background(AppTheme.colorScheme.jetGray, shape = AppTheme.shape.rounded10)
-                            .width(280.dp)
-                            .padding(vertical = 4.dp)
-                    ) {
-                        suggestions.forEachIndexed { index, suggestion ->
-                            SuggestionItem(
-                                suggestion = suggestion,
-                                onClick = {
-                                    onValueChange(suggestion)
-                                    focusManager.clearFocus()
-                                    keyboardController?.hide()
-                                    showSuggestions = false
-                                    onSearch(query)
-                                }
-                            )
-                        }
-                    }
-                }
-            }
+            endIcon()
         }
-
     }
 }
 
@@ -234,7 +202,7 @@ fun SuggestionItem(
     Text(
         text = suggestion,
         style = AppTheme.typography.inputText,
-        color = Color.White,
+        color = AppTheme.colorScheme.immutableWhite,
         modifier = Modifier
             .fillMaxWidth()
             .clickable(onClick = onClick)
