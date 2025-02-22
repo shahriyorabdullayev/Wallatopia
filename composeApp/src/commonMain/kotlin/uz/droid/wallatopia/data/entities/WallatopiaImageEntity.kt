c269ec1b3e389f41ffd6278995c07c9ac1396cfd
shahriyor <shahriyorabdullayev2000@gmail.com>
2025-02-22 11:09:32 +0500

    duplicate ai generated image fixed

--- composeApp/src/commonMain/kotlin/uz/droid/wallatopia/data/entities/WallatopiaImageEntity.kt
+++ composeApp/src/commonMain/kotlin/uz/droid/wallatopia/data/entities/WallatopiaImageEntity.kt
@@ -9,13 +9,13 @@ const val WALLATOPIA_IMAGES_TABLE = "wallatopia_images"
 
 @Entity(tableName = WALLATOPIA_IMAGES_TABLE)
 data class WallatopiaImageEntity(
-    @PrimaryKey
     @ColumnInfo(name = "id")
     val id:String,
     @ColumnInfo("timestamp")
     val timestamp: Long,
     @ColumnInfo(name = "thumb_url")
     val thumbUrl: String,
+    @PrimaryKey
     @ColumnInfo(name = "original_url")
     val originalUrl: String,
     @ColumnInfo(name = "is_favorite")
