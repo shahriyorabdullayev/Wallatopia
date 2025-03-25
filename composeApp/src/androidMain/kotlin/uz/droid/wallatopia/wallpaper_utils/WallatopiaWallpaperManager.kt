618f254f5be31ae48db0d45527009c08c34359ae
JonibekXolmonov <pendragon05082002@gmail.com>
2025-03-25 17:35:04 +0500

    wallpaper set process optimisation

--- composeApp/src/androidMain/kotlin/uz/droid/wallatopia/wallpaper_utils/WallatopiaWallpaperManager.kt
+++ composeApp/src/androidMain/kotlin/uz/droid/wallatopia/wallpaper_utils/WallatopiaWallpaperManager.kt
@@ -3,27 +3,41 @@ package uz.droid.wallatopia.wallpaper_utils
 import android.app.WallpaperManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class WallatopiaWallpaperManager(
     private val wallpaperManager: WallpaperManager
 ) {
 
-    fun setOnHomeScreen(imageByteArray: ByteArray, onSuccess: () -> Unit) {
-        val bitmap = imageByteArray.convertToBitmap()
-        wallpaperManager.setBitmap(bitmap, null, false, WallpaperManager.FLAG_SYSTEM)
-        onSuccess()
+    suspend fun setOnHomeScreen(imageByteArray: ByteArray, onSuccess: () -> Unit) {
+        withContext(Dispatchers.IO) {
+            val bitmap = imageByteArray.convertToBitmap()
+            wallpaperManager.setBitmap(bitmap, null, false, WallpaperManager.FLAG_SYSTEM)
+            withContext(Dispatchers.Main) {
+                onSuccess()
+            }
+        }
     }
 
-    fun setOnLockScreen(imageByteArray: ByteArray, onSuccess: () -> Unit) {
-        val bitmap = imageByteArray.convertToBitmap()
-        wallpaperManager.setBitmap(bitmap, null, false, WallpaperManager.FLAG_LOCK)
-        onSuccess()
+    suspend fun setOnLockScreen(imageByteArray: ByteArray, onSuccess: () -> Unit) {
+        withContext(Dispatchers.IO) {
+            val bitmap = imageByteArray.convertToBitmap()
+            wallpaperManager.setBitmap(bitmap, null, false, WallpaperManager.FLAG_LOCK)
+            withContext(Dispatchers.Main) {
+                onSuccess()
+            }
+        }
     }
 
-    fun setOnBothScreens(imageByteArray: ByteArray, onSuccess: () -> Unit) {
-        val bitmap = imageByteArray.convertToBitmap()
-        wallpaperManager.setBitmap(bitmap, null, false, WallpaperManager.FLAG_SYSTEM)
-        onSuccess()
+    suspend fun setOnBothScreens(imageByteArray: ByteArray, onSuccess: () -> Unit) {
+        withContext(Dispatchers.IO) {
+            val bitmap = imageByteArray.convertToBitmap()
+            wallpaperManager.setBitmap(bitmap, null, false, WallpaperManager.FLAG_SYSTEM)
+            withContext(Dispatchers.Main) {
+                onSuccess()
+            }
+        }
     }
 
     private fun ByteArray.convertToBitmap(): Bitmap {
